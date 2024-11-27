a14d35e6d874e7371c77393b0489d9fa54eb5140
Kevin <jekevin964@gmail.com>
2024-11-26 23:17:23 -0500

    add test colorscheme

--- config/themes/test.nix
+++ config/themes/test.nix
@@ -1,18 +1,20 @@
+# Author: Me
+# Ok so thats why its
 {
-  base00 = "#e0e9ff";
-  base01 = "#cad2e6";
-  base02 = "#b3bacc";
-  base03 = "#9da3b3";
-  base04 = "#868c99";
-  base05 = "#4b4e55";
-  base06 = "#4b4e55";
-  base07 = "#4b4e55";
-  base08 = "#cc6165";
-  base09 = "#cc9661";
-  base0A = "#cc9661";
-  base0B = "#61cc61";
-  base0C = "#6196cc";
-  base0D = "#6161cc";
+  base00 = "#fffefd";
+  base01 = "#e6e5e4";
+  base02 = "#cccbca";
+  base03 = "#b3b2b1";
+  base04 = "#999898";
+  base05 = "#5c5c5c";
+  base06 = "#5c5c5c";
+  base07 = "#5c5c5c";
+  base08 = "#b66467";
+  base09 = "#b88757";
+  base0A = "#b88757";
+  base0B = "#76b05c";
+  base0C = "#7d9ea7";
+  base0D = "#66858f";
   base0E = "#9661cc";
-  base0F = "#868c99";
+  base0F = "#aaa9a9";
 }
