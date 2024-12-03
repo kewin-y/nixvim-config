717e96e03c5c2241c4fc91e6bc4c3f4545b16832
Kevin <jekevin964@gmail.com>
2024-12-03 01:23:46 -0500

    fix yoru theme
    brightened base03, making comments readable

--- config/themes/yoru.nix
+++ config/themes/yoru.nix
@@ -3,8 +3,8 @@
   base00 = "#0c0e0f";
   base01 = "#121415";
   base02 = "#161819";
-  base03 = "#1f2122";
-  base04 = "#27292a";
+  base03 = "#27282a";
+  base04 = "#343637";
   base05 = "#edeff0";
   base06 = "#e4e6e7";
   base07 = "#f2f4f5";
