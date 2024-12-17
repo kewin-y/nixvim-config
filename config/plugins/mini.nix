8746fd0943d3177bd293eca0a2b073db3a51bc00
Kevin <jekevin964@gmail.com>
2024-12-17 10:09:58 -0500

    mini starter :3

--- config/plugins/mini.nix
+++ config/plugins/mini.nix
@@ -9,7 +9,33 @@ in {
       };
       starter = {
         evaluate_single = true;
-        header = ''Nevoim'';
+        header = ''
+           _______
+          < work! >
+           -------
+                  \   ^__^
+                   \  (oo)\_______
+                      (__)\       )\/\
+                          ||----w |
+                          ||     ||
+        '';
+        footer = "";
+        content_hooks = {
+          "__unkeyed-3.padding" = {
+            __raw = "require('mini.starter').gen_hook.aligning('center', 'center')";
+          };
+        };
+        items = {
+          "__unkeyed-1.buildtin_actions" = {
+            __raw = "require('mini.starter').sections.builtin_actions()";
+          };
+          "__unkeyed-2.recent_files" = {
+            __raw = "require('mini.starter').sections.recent_files(5, true, false)";
+          };
+          "__unkeyed-3.recent_files_current_directory" = {
+            __raw = "require('mini.starter').sections.recent_files(5, false, false)";
+          };
+        };
       };
     };
   };
